5b4973b50f8ac938ee16c7406e45fcbf3c533d4e
Ryslsn <fiesta-tm@mail.ru>
2022-11-12 14:00:26 +1000

    работа с проектом

--- js/script.js
+++ js/script.js
@@ -30,5 +30,51 @@ function showTabContent(i=0) {
         });
      }
     });
+    // -------------------------------------------------------------
+    const triger = document.querySelector('.btn_white ');
+    const modalokno = document.querySelector('.modal');
+    const modalclose = document.querySelector('.modal__close');
+    modalclose.addEventListener('click',()=>{
+        modalokno.style.display = "none";
+    });
+    triger.addEventListener('click',()=> {
+        modalokno.style.display = "block";
+    }) ; 
+// ---------------------------------------------------------------------
 
-});
+// ----------------------------------------------------
+
+const finishDate = '2022-12-13';
+function getTimeOstalos(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(t/(1000*60*60*24));
+    const hours = Math.floor((t/(1000*60*60)) % 24);
+    const minutes = Math.floor((t/(1000*60))%60);
+    const seconds = Math.floor((t/1000)%60);
+    return {
+      'totalSec':t,
+      'days': days,
+      'hours': hours,
+      'minutes' : minutes,
+      'seconds': seconds };
+    }
+function setClock(seceltor,endtime){
+   const timer = document.querySelector(seceltor),
+   days = timer.querySelector('#days'),
+   hours = timer.querySelector('#hours'),
+   minutes = timer.querySelector('#minutes'),
+   seconds = timer.querySelector('#seconds'),
+   timeInterval = setInterval(upDateClock,1000);
+   
+
+function upDateClock () {
+    const n = getTimeOstalos(endtime);
+    days.innerHTML = n.days;
+    hours.innerHTML = n.hours;
+    minutes.innerHTML = n.minutes;
+    seconds.innerHTML = n.seconds;
+    if(t.total <= 0) { clearInterval(timeInterval);}
+}
+}
+setClock('.timer',finishDate);
+    });
