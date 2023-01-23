ace8da2281f38fc344f35e080a1928d7260aece4
Ryslsn <fiesta-tm@mail.ru>
2023-01-23 15:02:08 +1000

    foodproect

--- js/script.js
+++ js/script.js
@@ -1,80 +1,206 @@
-window.addEventListener('DOMContentLoaded', () => {
-const tabs = document.querySelectorAll('.tabheader__item');
-const tabsContent = document.querySelectorAll ('.tabcontent');
-const tabsParent = document.querySelector('.tabheader__items');
-// ------------------------------------------------------------------
-function hideTabContent (item){
-tabsContent.forEach((item)=>{
-item.style.display = "none";
-});
-tabs.forEach(item => {
+window.addEventListener('DOMContentLoaded', function() {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+	
+function hideTabContent () {
+    tabsContent.forEach((item)=>{
+item.style.display="none";
+    });
+    tabs.forEach((item)=>{
 item.classList.remove('tabheader__item_active');
-});
+    });
 }
-// -------------------------------------------------------------
-function showTabContent(i=0) {
-    tabsContent[i].style.display = "block";
+function showTabContent (i){
+    tabsContent[i].style.display="block";
     tabs[i].classList.add('tabheader__item_active');
 }
-    hideTabContent();
-    showTabContent();
-    // 
-    tabsParent.addEventListener('click',(event)=>{
-        const target = event.target;
-     if (target && target.classList.contains('tabheader__item')) {
-        tabs.forEach((item,i)=>{
- if(item==target) {
-    hideTabContent();
+hideTabContent ();
+showTabContent(0);
+tabsParent.addEventListener('click',(event)=>{
+const target=event.target;
+if(target&&target.classList.contains('tabheader__item')){
+    tabs.forEach((item,i)=>{
+if(target==item){
+    hideTabContent ();
     showTabContent(i);
- }
-        });
-     }
+}
     });
-    // -------------------------------------------------------------
-    const triger = document.querySelector('.btn_white ');
-    const modalokno = document.querySelector('.modal');
-    const modalclose = document.querySelector('.modal__close');
-    modalclose.addEventListener('click',()=>{
-        modalokno.style.display = "none";
-    });
-    triger.addEventListener('click',()=> {
-        modalokno.style.display = "block";
-    }) ; 
-// ---------------------------------------------------------------------
+}
+});
+// // ------------------------------------------------------------------
+// const finishDate = '2023-01-27';
+// function getRaznizaTime (vrema) {
+//     const itogTime = Date.parse(vrema) - Date.parse(new Date());
+//         const getDay = Math.floor (itogTime/(1000*60*60*24) );
+//         const getHour = Math.floor (itogTime/(1000*60*60)%24);
+//         const getMinute = Math.floor (itogTime/(1000*60)%60);
+//         const getSecond = (Math.floor (itogTime/1000) %60);
+//         return {
+//             'total':itogTime,
+//             'dni':getDay,
+//             'chasi':getHour,
+//             'minyti':getMinute,
+//             'sekyndi':getSecond
+//         };
+// }
+// getRaznizaTime(finishDate);
 
-// ----------------------------------------------------
+// function setTime(selector,andTime) {
+// const timer = document.querySelector(selector);
 
-const finishDate = '2022-12-13';
-function getTimeOstalos(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date());
-    const days = Math.floor(t/(1000*60*60*24));
-    const hours = Math.floor((t/(1000*60*60)) % 24);
-    const minutes = Math.floor((t/(1000*60))%60);
-    const seconds = Math.floor((t/1000)%60);
-    return {
-      'totalSec':t,
-      'days': days,
-      'hours': hours,
-      'minutes' : minutes,
-      'seconds': seconds };
+// const day = timer.querySelector('#days');
+// const hour = timer.querySelector('#hours');
+// const minute = timer.querySelector('#minutes');
+// const second = timer.querySelector('#seconds');
+// timeInterval = setInterval(updateClock,1000);
+
+// function updateClock(){
+//     const itogTime = getRaznizaTime(finishDate);
+// day.innerHTML = itogTime.dni;
+// hour.innerHTML = itogTime.chasi;
+// minute.innerHTML = itogTime.minyti;
+// second.innerHTML = itogTime.sekyndi ;
+// }
+
+
+// }
+// setTime('.timer',finishDate);
+
+
+const finishDate = "2023-08-26";
+
+function getRaznizaTime ( vrema) {
+    const t = Date.parse(vrema) - Date.parse(new Date());
+   const day = Math.floor(t/(1000*60*60*24));
+   const hour = Math.floor(t/(1000*60*60)%24);
+   const minute = Math.floor(t/(1000*60)%60);
+   const second = Math.floor((t/1000)%60);
+
+   return {
+'total':t,
+'day':day,
+'hour':hour,
+'minute':minute,
+'second':second
+
+   }; }
+   function getZero(num) {
+    if (num >= 0 && num <10) {
+        return `0${num}`;
+    } else  {
+        return num;}
+   }
+   function setTime (selector,endTime){
+    const timer = document.querySelector(selector);
+    const dayy = timer.querySelector('#days');
+    const hourr = timer.querySelector('#hours');
+    const minutee = timer.querySelector('#minutes');
+    const secondd = timer.querySelector('#seconds');
+    updateClock();
+    const interval = setInterval(updateClock,1000);
+    function updateClock(){
+        const togo = getRaznizaTime(endTime);
+        dayy.innerHTML = getZero (togo.day);
+        hourr.innerHTML=togo.hour;
+        minutee.innerHTML=togo.minute;
+        secondd.innerHTML=togo.second;
+        if (togo.total <=0) {
+            clearInterval(interval);
+        }
     }
-function setClock(seceltor,endtime){
-   const timer = document.querySelector(seceltor),
-   days = timer.querySelector('#days'),
-   hours = timer.querySelector('#hours'),
-   minutes = timer.querySelector('#minutes'),
-   seconds = timer.querySelector('#seconds'),
-   timeInterval = setInterval(upDateClock,1000);
+   }
+  
    
+setTime('.timer',finishDate); 
+        //   ------------------------------------------------------
 
-function upDateClock () {
-    const n = getTimeOstalos(endtime);
-    days.innerHTML = n.days;
-    hours.innerHTML = n.hours;
-    minutes.innerHTML = n.minutes;
-    seconds.innerHTML = n.seconds;
-    if(t.total <= 0) { clearInterval(timeInterval);}
+        const callWhithUs = document.querySelectorAll('[data-mod]');
+        const modalWind = document.querySelector('.modal');
+        const modalClose = document.querySelector('[data-close]');
+        // --------------------------------------------------------------
+ function closeOkno () {
+            modalWind.style.display="none";
+            document.body.style.overflow="";
+        }
+function showOkno (){
+    modalWind.style.display="block";
+    document.body.style.overflow="hidden";
 }
+ callWhithUs.forEach((item)=>{
+            item.addEventListener('click', ()=>{
+            showOkno();
+});
+        });
+modalClose.addEventListener('click',()=>{ 
+    closeOkno();   });
+ document.addEventListener('keydown',(e)=>{
+        if (e.code==="Escape") {
+    closeOkno();
+}    
+        });
+
+      
+        // -------------------------------------------------------------
+class Karta {
+    constructor(img,h1,p,price,parentSelector,...res) {
+            this.img = img;
+            this.h1=h1;
+            this.p=p;
+            this.price=price;
+            this.parent = document.querySelector(parentSelector);
+            this.clas=res;
+    }
+    plusKart(){
+            const element = document.createElement('div');
+            if(this.clas.length===0){
+                element.classList.add('menu__item');
+            } else {   this.clas.forEach(item=>{
+                element.classList.add(item);
+                            });}
+         
+            element.innerHTML = ` <div>
+        <img src=${this.img} alt="vegy">
+        <h3 class="menu__item-subtitle">${this.h1}</h3>
+        <div class="menu__item-descr">${this.p}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>`;
+        this.parent.append(element);
+    }
 }
-setClock('.timer',finishDate);
-    });
+ new Karta (
+    "img/tabs/vegy.jpg",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    '395',
+    '.menu .container',
+    
+).plusKart();
+new Karta (
+    "img/tabs/elite.jpg",
+    'Меню "Премиум"',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    '780',
+    '.menu .container',
+   
+).plusKart();
+new Karta (
+    "img/tabs/post.jpg",
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    '410',
+    '.menu .container',
+    
+).plusKart();
+ 
+        // ----------------------------------------------------------------
+
+        
+});
+// ----------------------------------------------
+
+
